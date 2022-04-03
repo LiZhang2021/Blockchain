36785af30d7ecd97915158619eda3ff46dd4c1d2
LiZhang2021 <649895435@qq.com>
2022-04-03 22:46:57 +0800

    完成简单的CSMA_CA_Algorithm

--- Sources/测试小程序/Test.py
+++ Sources/测试小程序/Test.py
@@ -1,51 +1,8 @@
 import random
-import numpy as np
+import collections
 
-a = [1,2,3]
-c = 7
-for i in range(4):
-    k = sum(a[:i])
-    print(k)
-for i in range(4):
-    low = sum(a[:i])
-    high = sum(a[:i+1])
-    if c>=low and c<high:
-        print(i)
-
-
-
-alphas = range(1,10)
-print(alphas)
-beta = np.arange(0, 16, 2)
-print(beta)
-K = random.choice(beta)
-print(K)
-print(beta)
-
-beta = [1, 2, 6, 9, 11]
-m = np.mean(beta)
-print(m)
-n = np.median(beta)
-print(n)
-
-bmax = max(beta)
-print(bmax)
-
-P =beta[0]
-print("P", P)
-
-Disk = [0]
-sum_p = 0
-for p in beta:
-    sum_p = sum_p + p
-    Disk.append(sum_p)
-print(beta)
-print(Disk)
-
-
-p1 = random.random()
-print("random", p1)
-p2 = random.randint(0, 300)
-print("randint", p2)
-p3 = random.uniform(0, 300)
-print("uniform", p3)
+list = ['1', '2', '3']
+list = collections.deque(list)
+print(list)
+list.popleft()
+print(list)
